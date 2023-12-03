68a57e1c72aed019041446fc6bc1ea13be476aec
Alonso <alonsoortiz73@gmail.com>
2023-12-03 12:31:26 -0800

    Fix type error on weekly-fixtures Adjust cron job time

--- app/ui/weekly-fixtures.tsx
+++ app/ui/weekly-fixtures.tsx
@@ -2,10 +2,11 @@ import getFixtures from '@/app/lib/data';
 import FixtureCard from '@/app/ui/fixture-card';
 import type { FixtureCardProps } from '@/app/ui/fixture-card';
 
+
 export default async function WeeklyFixtures() {
 
     let fixtures = await getFixtures();
-    fixtures = fixtures.sort((a: Date, b: Date) => new Date(a.date) - new Date(b.date));
+    fixtures = fixtures.sort((a: any, b: any) => new Date(a.date).getTime() - new Date(b.date).getTime());
 
     return (
         <div className="flex flex-col items-center justify-center w-screen space-y-4">
