a1884463acaffb7fb9c5df798758423f01b11eba
Alonso <alonsoortiz73@gmail.com>
2023-12-03 12:22:47 -0800

    Fix headers issue

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,16 +1,19 @@
 export default async function getFixtures() {
   try {
-    const response = await fetch(`${process.env.SUPABASE_PUBLIC_URL}/rest/v1/fixtures`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        apikey: process.env.SUPABASE_ANON_KEY,
-      },
-    })
+    const requestHeaders: HeadersInit = new Headers();
+    requestHeaders.set("Content-Type", "application/json");
+    requestHeaders.set("apikey", process.env.SUPABASE_ANON_KEY!);
+
+    const response = await fetch(
+      `${process.env.SUPABASE_PUBLIC_URL}/rest/v1/fixtures`,
+      {
+        method: "GET",
+        headers: requestHeaders,
+      }
+    );
 
     const fixtures = await response.json();
     return fixtures;
-
   } catch (error) {
     console.error("getFixtures error: ", error);
     throw new Error("Failed to get fixtures from Supabase");
